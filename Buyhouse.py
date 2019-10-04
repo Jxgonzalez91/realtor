4637d74edda7c96f5069fec4367c6b1960acd6b7
Jxgonzalez91 <55545686+Jxgonzalez91@users.noreply.github.com>
2019-10-04 15:24:56 -0400

    Add files via upload

--- Buyhouse.py
+++ Buyhouse.py
@@ -1,14 +1,23 @@
-houseList=[]
+houseList = []
 class BuyHouse:
-     def _init_(self, property1=None, property2=None, property3=None, property4=None, property5=None):
-         self.property1 = property1
-         self.property2 = property2
-         self.property3 = property3
-         self.property4 = property4
-         self.property5 = property5
+    def _init_(self, property1=None, property2=None, property3=None, property4=None, property5=None):
+        self.property1 = property1
+        self.property2 = property2
+        self.property3 = property3
+        self.property4 = property4
+        self.property5 = property5
+
+
+rang = float(input("How many properties would you like to purchase?  >>"))
+for x in range(rang):
+    buy = House()
+    buy.property = input("Enter Property >> ")
+    buy.price = input("Enter Price >> ")
+
 houseListCounter = 1
-for x  in  houseList
+for h in houseList:
     print(f"House #{houseListCounter}:")
     print(f"""
-    purchasedProperty ............ {x.purchasedProperty}
-    purchasePrice ............... {x.purchasePrice}""")
+    purchasedProperty ............ {x.property}
+    purchasePrice ............... {x.price}""")
+    houseList += 1
--- Sellhouse.py
+++ Sellhouse.py
@@ -1,13 +1,23 @@
 class SoldHouse:
-     def _init_(self, property1=None, property2=None, property3=None, property4=None, property5=None):
-         self.property1 = property1
-         self.property2 = property2
-         self.property3 = property3
-         self.property4 = property4
-         self.property5 = property5
+    def _init_(self, property1=None, property2=None, property3=None, property4=None, property5=None):
+        self.property1 = property1
+        self.property2 = property2
+        self.property3 = property3
+        self.property4 = property4
+        self.property5 = property5
+
+
+rang = float(input("How many properties would you like to sell?  >>"))
+for x in range(rang):
+    sold = House()
+    sold.property = input("Enter Property >> ")
+    sold.price = input("Enter Price >> ")
+
+
 houseListCounter = 1
-for x  in  houseList
+for h in houseList:
     print(f"House #{houseListCounter}:")
     print(f"""
-    soldProperty ............ {x.soldProperty}
-    soldPrice ............... {x.soldPrice}""")
+    soldProperty ............ {x.property}
+    soldPrice ............... {x.Price}""")
+    houseList += 1
