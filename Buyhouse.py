838b9d289b6942c2dd343e75f7796e57fee1271d
Jxgonzalez91 <55545686+Jxgonzalez91@users.noreply.github.com>
2019-10-04 15:10:29 -0400

    Add files via upload

---
+++ Buyhouse.py
@@ -0,0 +1,14 @@
+houseList=[]
+class BuyHouse:
+     def _init_(self, property1=None, property2=None, property3=None, property4=None, property5=None):
+         self.property1 = property1
+         self.property2 = property2
+         self.property3 = property3
+         self.property4 = property4
+         self.property5 = property5
+houseListCounter = 1
+for x  in  houseList
+    print(f"House #{houseListCounter}:")
+    print(f"""
+    purchasedProperty ............ {x.purchasedProperty}
+    purchasePrice ............... {x.purchasePrice}""")
